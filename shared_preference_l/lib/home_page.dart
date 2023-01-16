606f179eef64bb063c8bc95fb16a91c14a898489
GungDeRaka <aswinraka4@gmail.com>
2023-01-16 20:43:26 +0800

    SharedPrefs part.1

--- shared_preference_l/lib/home_page.dart
+++ shared_preference_l/lib/home_page.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller =
+      TextEditingController(text: "No Name");
+  bool isOn = false;
+
+  void saveData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString("nama", _controller.text);
+    pref.setBool("ison", isOn);
+  }
+
+Future<String> loadNama ( ) async{
+  SharedPreferences pref =await SharedPreferences.getInstance();
+  return pref.getString("nama") ?? "No Name";
+}
+Future<bool> loadBool ( ) async{
+  SharedPreferences pref =await SharedPreferences.getInstance();
+  return pref.getBool("ison") ?? false;
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Column()),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //textfield with controller
+          TextField(
+            controller: _controller,
+          ),
+          //switch button
+          Switch(
+              value: isOn,
+              onChanged: (newValue) {
+                isOn = newValue;
+                setState(() {});
+              }),
+          //save pref button
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueGrey,
+            ),
+            onPressed: () {setState(() {
+              saveData();
+            });},
+            child: const Text("Save"),
+          ),
+          //load pref button
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueGrey,
+            ),
+            onPressed: () {
+              setState(() {
+                loadNama().then((value) => _controller.text = value);
+                loadBool().then((value) => isOn = value);
+              });
+            },
+            child: const Text("Load"),
+          ),
+        ],
+      )),
     );
   }
 }
